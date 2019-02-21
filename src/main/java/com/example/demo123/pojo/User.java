424d875bc840abbc52a1f28392d17ed1341a3f77
junlinworld <33860483+junlinworld@users.noreply.github.com>
2019-02-21 09:18:07 +0800

    Revert "user类设置getter setter"
    This reverts commit 9ec5f262e5a10ecda35bb552387f8039901afd03.

--- src/main/java/com/example/demo123/pojo/User.java
+++ src/main/java/com/example/demo123/pojo/User.java
@@ -6,36 +6,4 @@ public class User {
     String userPassword;
     int age;
     String tall;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTall() {
-        return tall;
-    }
-
-    public void setTall(String tall) {
-        this.tall = tall;
-    }
 }
