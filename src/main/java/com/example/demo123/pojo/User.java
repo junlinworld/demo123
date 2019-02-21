58b82f9d8b9b46501d1b4e5d9fdea22dc5e7b4e5
junlinworld <33860483+junlinworld@users.noreply.github.com>
2019-02-21 10:39:02 +0800

    完善user类setter getter

--- src/main/java/com/example/demo123/pojo/User.java
+++ src/main/java/com/example/demo123/pojo/User.java
@@ -3,4 +3,30 @@ package com.example.demo123.pojo;
 
 public class User {
     String userName;
+    String userPassword;
+    int age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
